aab20c2588297758474d3139a6bd9661d8f45ba4
Ebose Esekheilu <ebose.esekheilu@ucalgary.ca>
2025-07-23 23:13:26 -0600

    Added previously agreed on information for the trafficlight class

--- trafficlight.cpp
+++ trafficlight.cpp
@@ -3,3 +3,41 @@
 //
 
 #include "trafficlight.h"
+#include <chrono>
+#include <thread>
+//whenever we make the light we're gonna make a thread
+const int red_stoptime = 50;
+const int green_stoptime = 45;
+const int yellow_stoptime = 5;
+trafficlight::trafficlight(int dir, int start_state) {
+    this->direction = dir;
+    this->stopSignal = false;
+    if (start_state == 2) {this->current = Green;}
+    else {this->current = Red;}
+}
+void trafficlight::changecolour() {
+    while (!stopSignal) {
+        {
+            std::lock_guard<std::mutex> lock(mt);
+            if (current == Red) {current = Green;}
+            else if (current == Green) {current = Yellow;}
+            else {current = Red;}
+        }
+        std::this_thread::sleep_for(
+    current == Red ? std::chrono::seconds(red_stoptime) :
+    current == Green ? std::chrono::seconds(green_stoptime) :
+    std::chrono::seconds(yellow_stoptime)
+);
+    }
+}
+
+void trafficlight::stop() {this->stopSignal = true;}
+int trafficlight::getstate() {
+    std::lock_guard<std::mutex> lock(mt);
+    switch (this->current) {
+        case Red:return 0;
+        case Green:return 2;
+        case Yellow:return 1;
+        default: return -1;
+    }
+}
--- trafficlight.h
+++ trafficlight.h
@@ -4,13 +4,20 @@
 
 #ifndef TRAFFICLIGHT_H
 #define TRAFFICLIGHT_H
-
-
-
+#include <mutex>
 class trafficlight {
+private:
+    int direction; //1 = N, -1 = E
+    enum state {Red, Yellow, Green};
+    state current;
+    bool stopSignal;
+    std::mutex mt;//to protect the code from crashing as we need to getstate and change colour at the same time
+public:
+    trafficlight(int dir, int start_state); //start state is either red or green
+    void changecolour();
+    int getstate();
+    void stop();
 
 };
-
-
 
 #endif //TRAFFICLIGHT_H
