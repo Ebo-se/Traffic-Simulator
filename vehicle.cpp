3cce4ce29bc8d5e277e1de1899bf05c12f65f1ab
Siddharth <siddharthnarendrakum@ucalgary.ca>
2025-07-26 21:20:46 -0600

    attaching vehicle texture

--- vehicle.cpp
+++ vehicle.cpp
@@ -4,5 +4,23 @@
 
 #include "vehicle.h"
 
-vehicle::vehicle(int id, int dir) {this->id = id; this->direction = dir;}
+#include "SFML/Graphics/Texture.hpp"
+
+vehicle::vehicle(int dir) {this->direction = dir;}
+
+
 int vehicle::getdirection() {return direction;}
+
+void vehicle:: loadTexture()
+{
+    sf::Texture vehicleTexture;
+    if (!vehicleTextureLoaded)
+    {
+        if (!vehicleTexture.loadFromFile(".Graphic-elements/vehicle.png"))
+        {
+            throw std::runtime_error("Failed to load texture from Graphics-elements/vehicle.png");
+        }
+        vehicleTextureLoaded = true;
+    }
+}
+
--- vehicle.h
+++ vehicle.h
@@ -8,12 +8,14 @@
 class vehicle {
 private:
     int wait_time;
-    int id;
     int direction; //1=N,-1=E
     double speed = 35.7;
+    bool isRunning;
+    bool vehicleTextureLoaded = false;
 public:
-    vehicle(int id,int dir); //constructor
+    vehicle(int dir); //constructor
     int getdirection();
+    void loadTexture();
 };
 
 #endif //VEHICLE_H
